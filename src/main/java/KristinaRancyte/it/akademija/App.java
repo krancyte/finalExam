96ee37315a6032d86a73b811be3849c09ddd6a7a
krancyte <k.rancyte@gmail.com>
2017-06-26 11:44:53 +0300

    first

---
+++ src/main/java/KristinaRancyte/it/akademija/App.java
@@ -0,0 +1,13 @@
+package KristinaRancyte.it.akademija;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
